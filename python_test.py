2ac7bbb870cc3d2a4876e71d8baa32a264f6336d
sergey <morozoff_serg@mail.ru>
2020-12-07 13:21:33 +0300

    second commit

--- python_test.py
+++ python_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-import os
+#import os
 import re
 import ast
 import argparse
@@ -10,51 +10,97 @@ import datetime
 
 
 def print_welcome():
-	date = datetime.datetime.now().strftime("%B %d %Y")
-	print(f"Hello, {getpass.getuser()}! Today is {date}. Beautiful day for miracles!") 
+    date = datetime.datetime.now().strftime("%B %d %Y")
+    user = getpass.getuser()
+    print(f"Hello, {user}! Today is {date}. Beautiful day for miracles!")
+
+def eval_string(in_string):
+    try:
+        elements = ast.literal_eval(in_string)
+    except ValueError as error:
+        print(error, file=sys.stderr)
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    else:
+        return elements
+
+def open_file(name):
+    try:
+        file_object = open(name)
+        line = file_object.readline()
+    except IOError as error:
+        print(error, file=sys.stderr)
+        sys.exit(1)
+    else:
+        return line
+
+def open_file_w(name):
+    try:
+        out = open(name, 'w')
+    except IOError as error:
+        print(error, file=sys.stderr)
+        sys.exit(1)
+    else:
+        return out
 
 def main():
-	filename = 'string.txt'
-	with open(filename) as file_object:
-		contents = file_object.read()
-	elements = ast.literal_eval(contents)
-	print_welcome()	
-	print("Input string:\n", contents.rstrip())
-	print("Output tuples:")
-	tuples_union = []
-	numbers = []
-	for element in elements:
-		if isinstance(element, tuple):
-			for t in element:
-				tuples_union.append(t)
-		else:
-			numbers.append(element)
-	for number in numbers:
-		if len(tuples_union) >=  number:
-			print(tuple(tuples_union[:number]))
-			del tuples_union[:number]
-	
-				
+    if args.file is not None:
+        contents = open_file(args.file)
+    else:
+        contents = args.string
+    
+    elements = eval_string(contents)
+    
+    try:
+        out = open(args.out, 'w')
+    except IOError as error:
+        print(error, file=sys.stderr)
+        sys.exit(1)
+    
+    if args.out is not None:
+        out = open_file_w(args.out)
 
+    print_welcome()
+    print("Input string:\n", contents)
+    print("Output tuples:")
+   
+    tuples_union = []
+    if isinstance(elements, tuple):
+        for element in elements:
+            if isinstance(element, tuple):
+                for t in element:
+                    tuples_union.append(t)
+            elif isinstance(element, int) and element >= 0:
+                if len(tuples_union) >= element:
+                    print(tuple(tuples_union[:element]))
+                    if args.out is not None:
+                        print(tuple(tuples_union[:element]),file=out)
+                    del tuples_union[:element]
+                else:
+                    sys.exit(0)
+            else:
+                print(f"Incorrect input: {element} is not narutal number or tuple.", file=sys.stderr)
+                sys.exit(1)
+    elif isinstance(elements, int):
+        if elements == 0:
+            print(tuple())
+            sys.exit(0)
+        elif elements > 0:
+            sys.exit(0)
+        else:
+            print(f"Incorrect input: {elements} is not narutal number or tuple.", file=sys.stderr)
+            sys.exit(1)
+    else:
+        print(f"Incorrect input: {elements} is not narutal number or tuple.", file=sys.stderr)
+        sys.exit(1)
 
-
-
-
-#	print(contents.rstrip())
-#	match = re.search(r"\(.*?\)", contents)
-#	tuples = re.findall(r"\(.*?\)", contents)
-#	truncated = re.sub(r"\(.*?\)", '', contents)
-#	print('result after delete', truncated)
-#	t = tuple(truncated)
-#	print('tupled', t)
-#	s = ast.literal_eval(contents)
-#	print(s)
-	
+    if args.out is not None:
+        out.close() 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description = 'Magical pipeline.')
-	parser.add_argument('-f', '--file', help='input file')
-	parser.add_argument('-o', '--out', help='output file')
-#	parser.add_argument('-o', '--out', help='output file')
-	args = parser.parse_args()
-	main()
+    parser = argparse.ArgumentParser(description='Magical pipeline.')
+    parser.add_argument('-f', '--file', help='input file')
+    parser.add_argument('-o', '--out', help='output file')
+    parser.add_argument('-s', '--string', help='string to process')
+    args = parser.parse_args()
+    main()
